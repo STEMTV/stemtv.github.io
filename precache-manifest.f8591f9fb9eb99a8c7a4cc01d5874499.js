f527e8149d03e7628d2266b1ec37e25074067d42
STEMTV <47457506+STEMTV@users.noreply.github.com>
2019-03-19 11:11:09 -0400

    Add files via upload

---
+++ precache-manifest.f8591f9fb9eb99a8c7a4cc01d5874499.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "154612bacb4a819a081f",
+    "url": "/static/js/main.c847976a.chunk.js"
+  },
+  {
+    "revision": "ba264cd90c4d36546d5a",
+    "url": "/static/js/2.8f71a45e.chunk.js"
+  },
+  {
+    "revision": "154612bacb4a819a081f",
+    "url": "/static/css/main.fd90ab20.chunk.css"
+  },
+  {
+    "revision": "67052516c221eefd6ea44fd5d28a8339",
+    "url": "/index.html"
+  }
+];
